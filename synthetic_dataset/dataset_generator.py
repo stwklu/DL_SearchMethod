43882aa1b997b0d038c959ea26d04bb85d7ffdeb
weikailu <weikailu96@gmail.com>
2018-04-01 17:57:51 -0600

    gamma correction for dataset perturbation

--- synthetic_dataset/dataset_generator.py
+++ synthetic_dataset/dataset_generator.py
@@ -5,6 +5,9 @@ import time
 import random
 from tracker_utils import *
 from matplotlib import pyplot as plt
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
+from skimage.util import img_as_float
 
 # ground truth location drawn in green
 ground_truth_color = (0, 255, 0)
@@ -34,7 +37,7 @@ def get_bbox(frame, corners):
 
     return min_x, max_x, min_y, max_y
 
-def synthetic_dataset(frame, corners, num_samples=10):
+def synthetic_dataset(frame, corners, num_samples=10, dof=3, occlusion=False, light=False):
     # Normal distributions used for delta_p
     tx_samples = np.round(np.random.normal(loc=0, scale=5, size=num_samples))
     ty_samples = np.round(np.random.normal(loc=0, scale=5, size=num_samples))
@@ -52,6 +55,44 @@ def synthetic_dataset(frame, corners, num_samples=10):
         W = np.array([[scale_samples[i], 0, tx_samples[i]], [0, scale_samples[i], ty_samples[i]], [0, 0, 1]])
         new_corners = np.around(np.dot(W, corners)).astype(int)
 
+
+        # Adding occlusion and light-interference
+        # Parameters
+        epsilon = 1                 # Probability of perturbation
+        max_occlusion = 10          # Max number of occlusion regions
+        if occlusion and random.uniform(0,1)<epsilon:
+            n_gamma = random.randint(0,max_occlusion)
+            for i in range(n_gamma):
+                # Area of perturbation
+                height, width, channels = frame.shape 
+                width_1 = int(random.uniform(0, width))
+                width_2 = int(random.uniform(0, width))
+                height_1 = int(random.uniform(0, height))
+                height_2 = int(random.uniform(0, height))
+                width_range = [min(width_1, width_2), max(width_1, width_2)]
+                height_range = [min(height_1, height_2), max(height_1, height_2)]
+
+                # Gamma correction
+                gamma = random.uniform(0,1)
+                inv_gamma = 1/gamma
+                table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+                
+                # Perturb within area
+                #print(height_range[0],height_range[1], width_range[0],width_range[1])
+                perturb_area = frame[height_range[0]:height_range[1], width_range[0]:width_range[1]]
+                perturb_area = cv2.LUT(perturb_area, table)
+                frame[height_range[0]:height_range[1], width_range[0]:width_range[1]] = perturb_area
+
+
+        if light and random.randint(0,1)<epsilon:
+            pass
+
+        # Show image and exit for testing
+        plt.imshow(frame)
+        plt.show()
+        exit()
+
+
         #mce = math.sqrt(np.sum(np.square(corners - new_corners)) / 4)
         #print(mce)
 
@@ -64,6 +105,8 @@ def synthetic_dataset(frame, corners, num_samples=10):
         # Crop by synthetic corners
         try:
             min_x, max_x, min_y, max_y = get_bbox(frame, new_corners)
+            #[ top_left, down_left, down_right, top_right ]
+            four_pts = [(min_x, min_y),(max_x, min_y),(max_x, max_y),(min_x, max_y)]
             crop_frame = frame[min_y:max_y, min_x:max_x]
             crop_frame = cv2.resize(crop_frame, (224, 224))
         except:
@@ -73,11 +116,38 @@ def synthetic_dataset(frame, corners, num_samples=10):
             plt.imshow(frame)
             plt.show()
             exit()
+
+
+
+
+
+        # Random homography transform
+        if dof==8:
+            rho = 32
+            patch_size = 128
+            visualization = True
+
+            perturbed_4_pts = []
+            for pt in four_pts:
+                perturbed_4_pts.append((pt[0] + random.randint(-rho,rho), pt[1]+random.randint(-rho,rho)))
+                print((pt[0] + random.randint(-rho,rho), pt[1]+random.randint(-rho,rho)))
+            if visualization:
+                perturbed_annotated_image = frame.copy()
+                cv2.polylines(perturbed_annotated_image, np.int32([perturbed_4_pts]), 1, (0,0,0), thickness=5)
+                plt.imshow(perturbed_annotated_image)
+                plt.show()
+                exit()
+
         
         image_samples.append(crop_frame)
         delta_p_samples.append(np.array([tx_samples[i], ty_samples[i], scale_samples[i]]))
         previous_corners_samples.append(corners)
         corners_samples.append(new_corners)
+
+
+
+
+
     return np.array(image_samples), np.array(delta_p_samples), np.array(previous_image_samples), np.array(previous_corners_samples), np.array(corners_samples)
 
 if __name__ == '__main__':
@@ -117,7 +187,7 @@ if __name__ == '__main__':
                             np.append(ground_truths[i, 2:4], [1]),
                             np.append(ground_truths[i, 4:6], [1]),
                             np.append(ground_truths[i, 6:8], [1])]).T
-        images, delta_ps, prev_images, prev_corners, corners = synthetic_dataset(frame, corners)
+        images, delta_ps, prev_images, prev_corners, corners = synthetic_dataset(frame, corners, dof=3, occlusion=True, light=True)
         all_images.append(images)
         all_delta_ps.append(delta_ps)
         all_prev_images.append(prev_images)
